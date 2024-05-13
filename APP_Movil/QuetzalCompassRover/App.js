d9f0c0eb097ccf3a38f42eddb440c3e7ca50d53d
Osman Pérez <osmanperez66@gmail.com>
2024-05-13 11:30:33 -0600

    [ADD] Map

--- APP_Movil/QuetzalCompassRover/App.js
+++ APP_Movil/QuetzalCompassRover/App.js
@@ -1,18 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import MapScreen from './src/screens/maps/map.js';
+import { StyleSheet, Text, View } from "react-native";
+import Navigation from "./src/navigation";
 
 export default function App() {
   return (
-      <MapScreen />
+    <Navigation />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
